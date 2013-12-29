0c9de1b1b31ded62c0307055f7d8e0c2ff39955d
orbitz <orbitz@gmail.com>
2013-12-29 12:48:16 +0100

    Cleanup useless const decorators

--- kmer_utils.h
+++ kmer_utils.h
@@ -1,12 +1,11 @@
 // Kmer functions
-void convert_kmer_to_num(char *str, const unsigned long length);
-unsigned long num_to_index(const char *str, const int kmer, const long error_pos);
+void convert_kmer_to_num(char *str, unsigned long length);
+unsigned long num_to_index(const char *str, const int kmer, long error_pos);
 char *index_to_kmer(unsigned long long index, long kmer);
-unsigned long long * get_kmer_counts_from_file(const char *fn, const int kmer);
+unsigned long long * get_kmer_counts_from_file(FILE *fh, int kmer);
 
 // Utility functions
 char *strnstrip(const char *s, char *dest, int c, int len);
-inline unsigned long long pow_four(unsigned long long x);
+unsigned long long pow_four(unsigned long long x);
 
-// Variables
-const unsigned char alpha[256]; 
+extern const unsigned char alpha[256];
